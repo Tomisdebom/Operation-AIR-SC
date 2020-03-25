e8fc43c1f75b0dcb14a7fcd846b2345f4fd86cdb
Danielvanhanswijk <danielvanhanswijk@hotmail.com>
2020-03-25 09:29:29 +0100

    test

--- Init.m
+++ Init.m
@@ -1,12 +1,5 @@
 %parameter file
 
 tidal_volume =0.5; %tidal volume (L)
-respiratory_rate= 15;  % breaths/min
-IE_ratio=1/3; % Inspiration to Expiration Ratio
-Pa = 1.01325 ; % Atmospheric pressure
-Pex = 10*cmH2O_to_Pa/100000;
-Pin = 50*cmH2O_to_Pa/100000;
-Pex = 10;
-Pin = 50;
-Ppulse = Pa + Pex;
-PulseAmp = Pin - Pex;
+respiratory_rate= 35;  % breaths/min
+IE_ratio=1/2;  % Inspiration to Expiration R
