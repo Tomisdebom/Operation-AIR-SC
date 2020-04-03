4c71a1ec3b0cc825d443f7b198ff8188ea68b324
MikedePont <m.depont@hotmail.com>
2020-04-03 13:18:22 +0200

    Add init callback function Final

--- Init.m
+++ Init.m
@@ -2,7 +2,7 @@
 
 tidal_volume =0.5; %tidal volume (L)
 respiratory_rate= 35;  % breaths/min
-IE_ratio=1/2;  % Inspiration to Expira  tion R
+IE_ratio=1/2;  % Inspiration to Expiration R
 
 tank_HIGH = 350;
 tank_LOW = 300;
